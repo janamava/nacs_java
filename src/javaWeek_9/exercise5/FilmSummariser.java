8103488614ce47dc523b87640693a6dc9a412b47
janamava <aizhana.kamalova@gmail.com>
2019-01-17 13:14:54 +0100

    added comments

--- src/javaWeek_9/exercise5/FilmSummariser.java
+++ src/javaWeek_9/exercise5/FilmSummariser.java
@@ -8,14 +8,16 @@ public class FilmSummariser {
 
     private List<Film> films = new FilmReader().getFilms();
 
-    public List<Film> getHighestRating(){
+
+    //three films with the highest rating, sorted by rating
+    public List<Film> getCondition1() {
         return films.stream()
                 .sorted(Comparator.comparing(Film::getScore).reversed())
                 .limit(3)
                 .collect(Collectors.toList());
     }
-
-    public List<Film> getHighestRatingMoreThen3Hours(){
+    //three films with the highest rating that are longer than three hours, sorted by rating
+    public List<Film> getCondition2() {
         return films.stream()
                 .sorted(Comparator.comparing(Film::getScore).reversed())
                 .filter(e -> e.getRuntime() > 180)
@@ -23,15 +25,15 @@ public class FilmSummariser {
                 .collect(Collectors.toList());
 
     }
-
-    public List<Film> get4MostExpensive(){
+    // four most expensive films, sorted by budget
+    public List<Film> getCondition3() {
         return films.stream()
                 .sorted((e1, e2) -> e2.getBudget().compareTo(e1.getBudget()))
                 .limit(4)
                 .collect(Collectors.toList());
     }
-
-    public List<Film> get4MostExpensiveShorterThen90(){
+    // four most expensive films that are shorter than one hour and a half, sorted by budget
+    public List<Film> getCondition4() {
         return films.stream()
                 .sorted(Comparator.comparing(Film::getBudget).reversed())
                 .filter(e -> e.getRuntime() < 90)
@@ -39,8 +41,9 @@ public class FilmSummariser {
                 .collect(Collectors.toList());
 
     }
-
-    public List<Film> getCondition5(){
+    // four most rated films with a rating higher than 7 and a budget between 50.000 and 500.000 dollars,
+    // sorted by budget
+    public List<Film> getCondition5() {
         return films.stream()
                 .filter(e -> e.getScore() > 7.0)
                 .filter(e -> e.getBudget() > 50000 && e.getBudget() < 500000)
@@ -48,8 +51,8 @@ public class FilmSummariser {
                 .limit(4)
                 .collect(Collectors.toList());
     }
-
-    public List<Film> condition6(){
+    // three shortest movies, that have most votes and highest score
+    public List<Film> getCondition6() {
         return films.stream()
                 .sorted(Comparator.comparing(Film::getRuntime))
                 .sorted(Comparator.comparing(Film::getVoteCount).reversed())
@@ -58,7 +61,8 @@ public class FilmSummariser {
                 .collect(Collectors.toList());
     }
 
-    public List<Film> condition7(){
+    // ten most voted movies with the score more then 8, sorted by highest revenue
+    public List<Film> getCondition7() {
         return films.stream()
                 .sorted(Comparator.comparing(Film::getVoteCount).reversed())
                 .filter(e -> e.getScore() > 8)
